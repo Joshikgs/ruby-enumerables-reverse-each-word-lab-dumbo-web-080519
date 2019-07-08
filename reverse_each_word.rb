6b224f65ec0852f1f9407f02ad5167ed5d3d6724
Josh Nho <joshnho@gmail.com>
2019-07-08 04:17:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_array = string.split()
   new_string_array = string_array.each { |word| word.reverse }
-  new_string_array.join(' ')
+  new_string_array.join()
 end
 
 
